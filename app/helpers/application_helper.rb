94773f5471d9784836c38a03ac07de12faebcf69
Parker <placy@trinity.edu>
2019-06-04 15:10:39 -0500

    Layout up to user controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   #Returns full title for each page
-  def full_title(page_title = ' ')
+  def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
