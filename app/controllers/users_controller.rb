d68dcf6e6b3a939b9fa2594a26a50857bc0044af
Parker <placy@trinity.edu>
2019-06-06 15:20:29 -0500

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
+      flash[:success] = "Welcome to Parker's Sample App!"
     else
       render 'new'
     end
