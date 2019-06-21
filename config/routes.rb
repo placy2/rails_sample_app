97f09aee93fb6caaa95fd260c6e588150924d0d0
Parker <placy@trinity.edu>
2019-06-21 11:10:21 -0500

    Add following and feed

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
 
   root 'static_pages#home'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: :edit
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
